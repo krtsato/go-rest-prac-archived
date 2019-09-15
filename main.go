8beeb5c38a80b33fe039b874f85b6f1c23819a4d
krtsato <ev60596@meiji.ac.jp>
2019-09-15 14:22:19 +0900

    Fix display 'Hello World' on browser .

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	// 1. Create router .
+	router := mux.NewRouter().StrictSlash(true)
+	// 2. Relate URLs to functions
+	router.HandleFunc("/", home)
+	// 3. Start on specified port
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
 }
