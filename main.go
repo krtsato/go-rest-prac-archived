0f8ecbe5805189865f2bde4e317b922278f64441
krtsato <ev60596@meiji.ac.jp>
2019-09-15 14:45:33 +0900

    Add findAllUsers

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,15 +9,35 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type User struct {
+	Id        int
+	FirstName string
+	LastName  string
+}
+
 func main() {
-	// 1. Create router .
 	router := mux.NewRouter().StrictSlash(true)
-	// 2. Relate URLs to functions
 	router.HandleFunc("/", home)
-	// 3. Start on specified port
+	// Add new relation .
+	router.HandleFunc("/users", findAllUsers)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World")
+}
+
+func findAllUsers(w http.ResponseWriter, r *http.Request) {
+	// Data to be converted to JSON
+	var userList = []User{
+		User{Id: 1, FirstName: "Taro", LastName: "Yamada"},
+		User{Id: 2, FirstName: "Jiro", LastName: "Sato"},
+	}
+
+	response, _ := json.Marshal(userList)
+	// Set response headers
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+
 }
