32f63c57e217ef3f46ba9847805694469ab3171d
krtsato <ev60596@meiji.ac.jp>
2020-10-01 03:58:10 +0900

    chore: add trial mock data

--- internal/domain/repository/user/repository.go
+++ internal/domain/repository/user/repository.go
@@ -1,11 +1,11 @@
 package user
 
 import (
-	"database/sql"
+	"context"
 	"go-rest-sample/internal/domain/entity/user"
 )
 
 type Repository interface {
-	InsertUser(DB *sql.DB, userEntity *user.Entity) (*user.Entity, error)
-	SelectByUserID(DB *sql.DB, userID int) (*user.Entity, error)
+	InsertUser(ctx context.Context, userEntity *user.Entity) (*user.Entity, error)
+	SelectByUserID(ctx context.Context, userID int) (*user.Entity, error)
 }
--- internal/infrastructure/mariadb/user/persistence.go
+++ internal/infrastructure/mariadb/user/persistence.go
@@ -1,9 +1,10 @@
 package user
 
 import (
-	"database/sql"
+	"context"
 	"go-rest-sample/domain/entity/user"
 	"go-rest-sample/domain/repository/user"
+	"time"
 )
 
 type userPersistence struct{}
@@ -12,8 +13,9 @@ func NewUserPersistence() user.Repository {
 	return &userPersistence{}
 }
 
+/*
 // ユーザ登録
-func (up userPersistence) InsertUser(DB *sql.DB, userEntity *user.entity) error {
+func (up userPersistence) InsertUser(ctx context.Context, userEntity *user.entity) error {
 	stmt, err := DB.Prepare("INSERT INTO user(user_id, name, email) VALUES(?, ?, ?)")
 	if err != nil {
 		return err
@@ -23,7 +25,7 @@ func (up userPersistence) InsertUser(DB *sql.DB, userEntity *user.entity) error 
 }
 
 // userID によってユーザ情報を取得する
-func (up userPersistence) SelectByUserID(DB *sql.DB, userID int) (*user.Entity, error) {
+func (up userPersistence) SelectByUserID(ctx context.Context, userID int) (*user.Entity, error) {
 	row := DB.QueryRow("SELECT * FROM user WHERE id = ?", userID)
 	// row 型を golang で利用できる形にキャストする
 	return convertToUser(row)
@@ -40,4 +42,30 @@ func convertToUser(row *sql.Row) (*user.Entity, error) {
 		return nil, err
 	}
 	return &user, nil
+}
+*/
+
+func (up userPersistence) SelectAllUser(ctx context.Context, limit int) (*user.EntitySlice, error) {
+	// 一旦モックデータを返却する
+	user1 := user.Entity{
+		ID:        1,
+		ProfileID: 1,
+		Mail:      "hoge@email.com",
+		Phone:     "00011112222",
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		DeletedAt: nil,
+	}
+
+	user2 := user.Entity{
+		ID:        2,
+		ProfileID: 2,
+		Mail:      "fuga@email.com",
+		Phone:     "33344445555",
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		DeletedAt: nil,
+	}
+
+	return *user.EntitySlice{user1, user2}, nil
 }
