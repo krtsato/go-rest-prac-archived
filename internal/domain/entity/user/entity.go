f978426e6f45a98c0f249ff8123375b351d8f27c
krtsato <ev60596@meiji.ac.jp>
2020-10-01 03:57:25 +0900

    fix: remove the unnecessary func in the entity

--- internal/domain/entity/user/entity.go
+++ internal/domain/entity/user/entity.go
@@ -14,16 +14,3 @@ type Entity struct {
 }
 
 type EntitySlice []*Entity
-
-func GetUserByID(conn *db.Conn, id int) (*User, error) {
-	//インフラストラクチャレイヤの実装を利用する
-	dto, err := mariadb.GetUserByID(conn, id)
-	if err != nil {
-		return nil, err
-	}
-	u := &User{
-		Id:   dto.ID,
-		Name: dto.Name,
-	}
-	return u, nil
-}
